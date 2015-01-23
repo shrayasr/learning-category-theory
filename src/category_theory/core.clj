9c0f1c4c83bf2a41e21139441812dcf184fc752d
Shrayas <shrayasr@gmail.com>
2015-01-23 18:23:48 +0530

    making project run through 'lein run'

--- src/category_theory/core.clj
+++ src/category_theory/core.clj
@@ -1,6 +1,11 @@
-(ns category-theory.core)
+(ns category-theory.core
+  (:gen-class))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn -main 
+  []
+  (foo ""))
