df7fe28256c87ce1a1cc67026e458e4c30104a1f
Shrayas <shrayasr@gmail.com>
2015-01-23 19:01:08 +0530

    Solved challenge 1.2
    "Implement the composition function in your favorite language. It takes
    two functions as arguments and returns a function that is their
    composition."

--- src/category_theory/core.clj
+++ src/category_theory/core.clj
@@ -1,14 +1,18 @@
 (ns category-theory.core
   (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn id [& args]
   (identity args))
 
+(def add1 (partial + 1))
+
+(def sub5 (partial - 5))
+
+(defn compo [fn1 fn2]
+  (comp fn1 fn2))
+
 (defn -main 
   []
-  (println (id 1 2 3)))
+  (println (id 1 2 3))
+  (let [sub-and-add (compo add1 sub5)]
+    (println (sub-and-add 10))))
