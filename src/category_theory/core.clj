5bad5f25172deb6a01f55795f27df84656900c17
Shrayas <shrayasr@gmail.com>
2015-01-23 19:48:33 +0530

    Reducing to bare bones implementation. Solves 3.1
    - Tried to get too ahead of myself. Reducing to simple implementation
      with just one argument.
    
    "Write a program that tries to test that your composition function
    respects identity."

--- src/category_theory/core.clj
+++ src/category_theory/core.clj
@@ -1,18 +1,25 @@
 (ns category-theory.core
   (:gen-class))
 
-(defn id [& args]
-  (identity args))
+(defn iden [arg]
+  arg)
 
-(def add1 (partial + 1))
+(defn compose [fn1 fn2]
+  (fn [arg]
+    (fn1 (fn2 arg))))
 
-(def sub5 (partial - 5))
+(defn add1 [arg]
+  (+ 1 arg))
 
-(defn compo [fn1 fn2]
-  (comp fn1 fn2))
+(defn mul5 [arg]
+  (* 5 arg))
 
 (defn -main 
   []
-  (println (id 1 2 3))
-  (let [sub-and-add (compo add1 sub5)]
-    (println (sub-and-add 10))))
+  (println (iden 1))
+  (let [mul5-then-add1 (comp add1 mul5)]
+    (println (mul5-then-add1 10)))
+  (let [id-then-fn (comp add1 iden)]
+    (println (id-then-fn 10)))
+  (let [fn-then-id (comp iden add1)]
+    (println (fn-then-id 10))))
