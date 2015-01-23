2a15107ffa82621ddca3540a0d5dfa39d40371c8
Shrayas <shrayasr@gmail.com>
2015-01-23 18:21:04 +0530

    initial repo from "lein new"

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to category-theory
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/category_theory/core_test.clj
@@ -0,0 +1,7 @@
+(ns category-theory.core-test
+  (:require [clojure.test :refer :all]
+            [category-theory.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
